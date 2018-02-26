c758a86dd56e034938043293bf2476bf5e469130
tahbristol <tahbristol@gmail.com>
2018-02-25 22:16:22 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,98 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
 
-const App = () => {
-  return (
+const link = {
+	width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+const Navbar = () =>
+  <div>
+    <NavLink to="/" style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+
+
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+
+	<NavLink
+		to="/signup"
+		exact
+		style={link}
+		activeStyle={{
+			background: 'darkblue'
+		}}>Sign Up</NavLink>
+  </div>;
+
+
+const Home = () => <h1>Home!</h1>;
+
+const About = () => <h1>This is my about component!</h1>;
+
+
+const Login = () =>
+  <form>
+    <h1>Login</h1>
     <div>
+      <input type="text" name="username" placeholder="Username" />
+      <label htmlFor="username">Username</label>
     </div>
-  );
-};
+    <div>
+      <input type="password" name="password" placeholder="Password" />
+      <label htmlFor="password">Password</label>
+    </div>
+    <input type="submit" value="Login" />
+  </form>;
 
-ReactDOM.render(
-  <App />, 
+const signup = () =>
+	<form>
+		<h1>Sign Up</h1>
+		<div>
+			<input type="text" name="username" placeholder="Username" />
+			<label htmlFor="username">Username</label>
+		</div>
+		<div>
+			<input type="password" name="password" placeholder="Password" />
+			<label htmlFor="password">Password</label>
+		</div>
+		<div>
+			<input type="password" name="passwordConfirm" placeholder="Password Confirmation" />
+			<label htmlFor="passwordConfirm">Password Confirmation</label>
+		</div>
+		<input type="submit" value="Login" />
+	</form>
+
+ReactDOM.render((
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+			<Route exact path="/signup" component={signup} />
+    </div>
+  </Router>),
   document.getElementById('root')
 );
